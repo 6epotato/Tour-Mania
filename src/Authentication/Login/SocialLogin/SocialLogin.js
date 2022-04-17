c1266c915d15d6f678f23e11cfea26773048379b
6epotato <aarifurrahman93@gmail.com>
2022-04-17 23:43:22 +0600

    social login added

--- src/Authentication/Login/SocialLogin/SocialLogin.js
+++ src/Authentication/Login/SocialLogin/SocialLogin.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import google from "../../../images/logo/google.png"
 import facebook from "../../../images/logo/facebook.png"
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
+import { useNavigate } from 'react-router-dom';
 
 
 const SocialLogin = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const navigate = useNavigate();
+    let errorElement;
+    if (error) {
+        errorElement =
+            <div>
+                <p className='text-danger'>Error: {error.message}</p>
+            </div>
+
+    }
+
+    if (user) {
+        navigate('/home')
+    }
     return (
         <div>
             <div className='d-flex align-items-center'>
@@ -12,10 +29,11 @@ const SocialLogin = () => {
                 <div style={{ height: "1px" }} className='bg-secondary w-50'></div>
             </div>
             <div>
-                <button className='btn btn-danger w-50 d-block mx-auto'>
+                <button onClick={() => signInWithGoogle()} className='btn btn-danger w-50 d-block mx-auto'>
                     <img style={{ width: '40px' }} className='px-2' src={google} alt="" />
                     Google Sign In</button>
             </div>
+            {errorElement}
             <div className='my-3'>
                 <button className='btn btn-primary w-50  d-block mx-auto'>
                     <img className='px-2' src={facebook} alt="" />
